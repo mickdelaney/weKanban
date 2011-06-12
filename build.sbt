7d9b1a47c6963479239f410a033b5942cad2dfaa
mick delaney <mickdelaney@gmail.com>
2011-06-12 22:49:38 +0100

    Migrated to sbt 0.10

---
+++ build.sbt
@@ -0,0 +1,7 @@
+name := "weKanban"
+
+version := "1.0"
+
+organization := "claritysys.co.uk"
+
+scalaVersion := "2.9.0"
--- project/build/SimpleProjectDefinition.scala
+++
@@ -1,5 +0,0 @@
-import sbt._
-
-class SimpleProjectDefinition(info: ProjectInfo) extends DefaultProject(info) {
-	override def mainClass = Some("HelloWorld")
-}
